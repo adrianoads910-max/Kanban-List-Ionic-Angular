311e6ee50ae9cb940daa4f836272c3e351dbcab8
Adriano Souza <adrianoads910@gmail.com>
2025-11-16 21:05:02 -0300

    drag and drop

--- kanban-board/src/app/components/kanban-column/kanban-column.component.ts
+++ kanban-board/src/app/components/kanban-column/kanban-column.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/models/task';
 import { TaskCardComponent } from '../task-card/task-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-kanban-column',
   standalone: true,
   templateUrl: './kanban-column.component.html',
   styleUrls: ['./kanban-column.component.scss'],
-  imports: [TaskCardComponent],
+  imports: [
+    CommonModule,
+    TaskCardComponent
+  ],
 })
 export class KanbanColumnComponent {
 
@@ -15,19 +19,23 @@ export class KanbanColumnComponent {
   @Input() status!: Task['status'];
   @Input() tasks: Task[] = [];
 
+  // 🔧 Evento para editar tarefa
   @Output() editTask = new EventEmitter<Task>();
 
-  // 🔥 IMPORTANTE: tipar como status, não Event
+  // 🔥 Envia status da coluna onde caiu o drop
   @Output() dropped = new EventEmitter<Task['status']>();
 
+  // 🔥 Envia qual tarefa começou a ser arrastada
   @Output() startDrag = new EventEmitter<Task>();
 
+  // Permite arrastar por cima
   onDragOver(event: DragEvent) {
     event.preventDefault();
   }
 
+  // Quando solta uma tarefa na coluna
   onDrop(event: DragEvent) {
     event.preventDefault();
-    this.dropped.emit(this.status);   // 👈 manda o status para o kanban.page
+    this.dropped.emit(this.status);
   }
 }
