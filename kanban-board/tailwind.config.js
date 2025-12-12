e1cc79bfffa73ab8e05fb114768da8ac8606104c
Adriano Souza <adrianoads910@gmail.com>
2025-12-12 13:57:52 -0300

    tailwind igore

--- kanban-board/tailwind.config.js
+++ kanban-board/tailwind.config.js
@@ -2,26 +2,14 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
+    "./src/**/*.html",
+    "./src/**/*.ts",
+    "./src/app/**/*.{html,ts}",
   ],
-  // 🔥 O SEGREDO: 'safelist' força essas classes a existirem no site final
-  safelist: [
-    // Cores de fundo das colunas
-    'bg-blue-200', 'border-blue-400',
-    'bg-yellow-100', 'border-yellow-400',
-    'bg-green-100', 'border-green-600',
-    'bg-purple-100', 'border-purple-400',
-    
-    // Fundo geral e layout
-    'bg-gray-200', 
-    'text-gray-800',
-    'grid', 
-    'grid-cols-1', 
-    'md:grid-cols-2', 
-    'xl:grid-cols-4', 
-    'gap-4'
-  ],
+
   theme: {
     extend: {},
   },
+
   plugins: [],
 };
